0f31cdeacfaccc8705a3a93f45a253d7ab4712a5
nayelitrbl <144268264+nayelitrbl@users.noreply.github.com>
2024-10-23 15:26:30 -0500

    Update DSString.cpp

--- DSString.cpp
+++ DSString.cpp
@@ -8,3 +8,10 @@
  * but your class should store its length in a member variable. 
  * DDO NOT USE C-STRING FUNCTIONS <string.h> or <cstring>.
  */  
+//data is a pointer to a character array that contains the string with a \0 terminator
+ 
+DSString::DSString() : data{nullptr}{
+    data = new char[1];//creates data as an array of characters with len of 1
+    data[0] = '\0';    //since data is null the first element will be the terminator symbol
+    len = 0;
+}
