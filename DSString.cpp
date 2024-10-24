a085e2236090d204e1eef372d8e8b4002f1854f1
nayelitrbl <144268264+nayelitrbl@users.noreply.github.com>
2024-10-23 21:53:23 -0500

    Update DSString.cpp

--- DSString.cpp
+++ DSString.cpp
@@ -15,3 +15,32 @@ DSString::DSString() : data{nullptr}{
     data[0] = '\0';    //since data is null the first element will be the terminator symbol
     len = 0;
 }
+
+DSString::DSString(const char *c){
+    if(c == nullptr){      //if data is emptry 
+        data = new char[1];//creates data as an array of characters with len of 1
+        data[0] = '\0';    //assigns first element as terminator symbol since it is nullptr
+        len = 0;
+    }
+    else{
+        len = 0;
+        while(c[len] != '\0'){
+            len++;
+        }
+        data = new char[len + 1];
+        for(size_t i = 0; i < len; i++){ //loops through c
+            data[i] = c[i];             //takes element at i of c and copies it into data array 
+        }
+        data[len] = '\0';
+    }
+ 
+}
+ 
+DSString::DSString(const DSString &rhs){
+    len = rhs.len;
+    data = new char[len + 1];
+    for(size_t i = 0; i < len; i++){ //loops through c
+        data[i] = rhs[i];             //takes element at i of c and copies it into data array 
+    }
+    data[len] = '\0';
+}
