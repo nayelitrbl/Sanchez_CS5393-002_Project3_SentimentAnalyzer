2f86026bd534904f7cf308d469fef61a903ac019
nayelitrbl <144268264+nayelitrbl@users.noreply.github.com>
2024-10-24 23:05:42 -0500

    Update DSString.cpp

--- DSString.cpp
+++ DSString.cpp
@@ -4,10 +4,11 @@
  * Implement the functions defined in DSString.h. You may add more functions as needed
  * for the project. 
  *
- * Note that c-strings don's store an explicit length but use `\0` as the terminator symbol
+ * Note that c-strings don't store an explicit length but use `\0` as the terminator symbol
  * but your class should store its length in a member variable. 
- * DDO NOT USE C-STRING FUNCTIONS <string.h> or <cstring>.
+ * DO NOT USE C-STRING FUNCTIONS <string.h> or <cstring>.
  */  
+
 //data is a pointer to a character array that contains the string with a \0 terminator
  
 DSString::DSString() : data{nullptr}{
