4f948acbaac212db943ac645491533503d41caa9
nayelitrbl <144268264+nayelitrbl@users.noreply.github.com>
2024-10-24 23:41:09 -0500

    Create SentimentClassifier.cpp
    Basic outline of classifier functions and class

---
+++ SentimentClassifier.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <iomanip>
+#include "SentimentClassifier.h"
+#include "DSString.h"
+
+ 
+void SentimentClassifier::readingIn(const char* trainFile, const char* testFile){
+    
+}
+ 
+void SentimentClassifier::train(){
+    
+}
+ 
+void SentimentClassifier::test(char* results){
+    
+}
+//evaluate function
+void SentimentClassifier::getResults(char* answFile, char* accuracy){
+    
+}
+ 
