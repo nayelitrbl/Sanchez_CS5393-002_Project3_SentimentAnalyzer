a56abca0fbee39c4518d6197c55aeb79d08dfd0c
nayelitrbl <144268264+nayelitrbl@users.noreply.github.com>
2024-10-30 00:57:39 -0500

    update variables SentimentClassifier.h
    update the needed variables for readingIn(...) and test(...) functions

--- SentimentClassifier.h
+++ SentimentClassifier.h
@@ -9,15 +9,24 @@
 class SentimentClassifier
 {
     private:
-        std::map<DSString, int> data;           //tweet, tweet sentiment (# 4 or 0) 
+        std::map<DSString, int> data;                   //tweet, tweet sentiment (# 4 or 0) 
 
     public:
-        DSString id, date, q, user, tweet;      //DSStrings for the values in the csv
-        size_t senti;                           //sentiment value
-
+        DSString id, date, q, user, tweet;              //DSStrings for the values in the csv
+        size_t senti;                                   //sentiment value
+        //reads in the values for the training file and testing file
         void readingIn(const char* trainFile, const char* testFile);
+        std::vector<DSString> tokenTweets;              //trainFile tweet vector
+        std::vector<int> sentiVect;                     //trainFile sentiment vector
+        std::unordered_map<DSString, int> sentiMap;     //map for tweet ID (key) and sentiment value
+        std::vector<DSString> tweetVect;                //testFile tweet vector
+        std::vector<DSString> tweetIDs;                 //testFile ID vector
+        
         void train();
-        void test(char* results);               //tests using the file without the sentiment
-        void getResults(char* answFile, char* accuracy);     //returns the results from all functions, create accuracy file with math
+
+        void test(char* results);                       //tests using the file without the sentiment
+        std::vector<int> finalSentiment;                //output sentiment vector
+        
+        void getResults(char* answFile, char* accuracy);//returns the results from all functions, create accuracy file with math
 };
 #endif
