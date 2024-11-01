32e3cf446dec26f1b2411669dfdaa2d0e77c9474
nayelitrbl <144268264+nayelitrbl@users.noreply.github.com>
2024-11-01 12:12:19 -0500

    update calling SentimentClassifier functions

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
-
-
+#include <vector>
+#include <algorithm>
+ 
+#include "DSString.h"
+#include "SentimentClassifier.h"
+ 
 int main(int argc, char** argv)
 {  
+    //run with: ./sentiment ./data/train_dataset_20.csv ./data/test_dataset_20.csv ./data/test_dataset_sentiment_20.csv ./results.csv ./accuracy.txt
     std::cout << "I will be a sentiment analyzer!" << std::endl;
-
-    return 0;
+    SentimentClassifier classifier;
+    classifier.readingIn(argv[1], argv[2]);
+    classifier.train();
+    classifier.test(argv[3]);
+    classifier.getResults(argv[4], argv[5]);
+ 
+   return 0;
 }
